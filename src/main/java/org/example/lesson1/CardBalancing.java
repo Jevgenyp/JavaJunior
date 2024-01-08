4395a83dc20325e73bf7033f3d512f7c1f91c91a
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2024-01-08 10:04:20 +0100

    Lesson1 upd

---
+++ src/main/java/org/example/lesson1/CardBalancing.java
@@ -0,0 +1,38 @@
+package org.example.lesson1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class CardBalancing {
+    public static void main(String[] args) {
+        List<Double> prices = Arrays.asList(10.0, 20.0, 30.0, 40.0, 50.0);
+
+        cardBalancing(prices);
+    }
+
+    public static void cardBalancing(List<Double> prices) {
+        double total = prices.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        OptionalDouble average = prices.stream()
+                .mapToDouble(Double::doubleValue)
+                .average();
+
+        if (average.isPresent()) {
+            System.out.println("Средняя цена: " + average.getAsDouble());
+            System.out.println("Товары с ценой выше средней: " +
+                    prices.stream()
+                            .filter(price -> price > average.getAsDouble())
+                            .collect(Collectors.toList()));
+            System.out.println("Товары с ценой ниже средней: " +
+                    prices.stream()
+                            .filter(price -> price < average.getAsDouble())
+                            .collect(Collectors.toList()));
+        } else {
+            System.out.println("Товары в корзине отсутствуют.");
+        }
+    }
+}
